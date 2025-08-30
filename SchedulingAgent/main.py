c1ace7711f10fcb76c0afbe949938f30f9ae9631
Abdul Rahim Mirani <abdulrahimmirani@gmail.com>
2025-08-30 19:43:29 -0400

    Scheduling agent

--- SchedulingAgent/main.py
+++ SchedulingAgent/main.py
@@ -1,9 +1,34 @@
-def test_function(a ,b):
-    return a + b
 
-a = 1
-b = 2
 
-test = test_function(a, b)
-print(test)
+class Scheduling:
+    def __init__(self, name, email, phone, date, time, location):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.date = date
+        self.time = time
+        self.location = location
+
+
+    def calender_lookup(self, calender_id):
+        pass
+
+    def calender_schdule(self, calender_id):
+        pass
+
+    def calender_update(self, calender_id):
+        pass
+
+    def calender_delete(self, calender_id):
+        pass
+
+    def calender_create(self, calender_id):
+        pass
+
+    def calender_read(self, calender_id):
+        pass
+    
+
+
+    
 
