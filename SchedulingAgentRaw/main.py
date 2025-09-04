771909f56626faba6adac6820748c39d99f03241
Abdul Rahim Mirani <abdulrahimmirani@gmail.com>
2025-09-04 01:36:04 -0400

    added code to authorize application

--- SchedulingAgentRaw/main.py
+++ SchedulingAgentRaw/main.py
@@ -1,32 +1,42 @@
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 
 
-class Scheduling:
-    def __init__(self, name, email, phone, date, time, location):
-        self.name = name
-        self.email = email
-        self.phone = phone
-        self.date = date
-        self.time = time
-        self.location = location
+# If modifying these SCOPES, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
+def authenticate_google():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'SchedulingAgentRaw/credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
 
-    def calender_lookup(self, calender_id):
-        pass
+    service = build('calendar', 'v3', credentials=creds)
+    return service
 
-    def calender_schdule(self, calender_id):
-        pass
-
-    def calender_update(self, calender_id):
-        pass
-
-    def calender_delete(self, calender_id):
-        pass
-
-    def calender_create(self, calender_id):
-        pass
-
-    def calender_read(self, calender_id):
-        pass
+if __name__ == '__main__':
+    service = authenticate_google()
+    print("Successfully authenticated with Google Calendar API.")
+    # We will add the logic to list events here in the next step.
     
 
 
